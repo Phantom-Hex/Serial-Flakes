0197feec31d55e226be4ed04398c4f61c6eaee2b
Tony Thigpen <tonytatsumaki@live.com>
2016-11-16 23:29:51 -0500

    Added Devices DB

---
+++ Serial Flakes/Controllers/devicesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Serial_Flakes.Models;
+
+namespace Serial_Flakes.Controllers
+{
+    public class devicesController : Controller
+    {
+        private AkihabaraCode db = new AkihabaraCode();
+
+        // GET: devices
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.devices.ToListAsync());
+        }
+
+        // GET: devices/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            device device = await db.devices.FindAsync(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // GET: devices/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: devices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,device_name,device_code,device_SKU,device_description,device_pic")] device device)
+        {
+            if (ModelState.IsValid)
+            {
+                db.devices.Add(device);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(device);
+        }
+
+        // GET: devices/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            device device = await db.devices.FindAsync(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: devices/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,device_name,device_code,device_SKU,device_description,device_pic")] device device)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(device).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(device);
+        }
+
+        // GET: devices/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            device device = await db.devices.FindAsync(id);
+            if (device == null)
+            {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: devices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            device device = await db.devices.FindAsync(id);
+            db.devices.Remove(device);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
