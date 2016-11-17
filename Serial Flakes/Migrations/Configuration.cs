3deb0eef8a235b18e78beb8cd1c3bc23e44d9fce
Tony Thigpen <tonytatsumaki@live.com>
2016-11-16 23:18:35 -0500

    Enabled Migrations

---
+++ Serial Flakes/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Serial_Flakes.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Serial_Flakes.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Serial_Flakes.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Serial_Flakes.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
